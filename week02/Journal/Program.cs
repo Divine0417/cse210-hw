eee0e5f1fc76e8a2e20520d8d0a0373f9b10ccf0
Divine0417 <divineayobami179@gmail.com>
2025-05-08 09:51:35 +0100

    Done

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,42 +6,6 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
 
-        Console.WriteLine("What is your grade percentage? ");
-        string input = Console.ReadLine();
-        int gradePercentage = int.Parse(input);
-
-        string letterGrade = "";
-        if (gradePercentage >= 90)
-        {
-            letterGrade = "A";
-        }
-        else if (gradePercentage >= 80)
-        {
-            letterGrade = "B";
-        }
-        else if (gradePercentage >= 70)
-        {
-            letterGrade = "C";
-        }
-        else if (gradePercentage >= 60)
-        {
-            letterGrade = "D";
-        }
-        else
-        {
-            letterGrade = "F";
-        }
-
-        Console.WriteLine($"Your grade is: {letterGrade}");
-
-        if (gradePercentage >= 60)
-        {
-            Console.WriteLine("Congratulations! You passed the course.");
-        }
-        else
-        {
-            Console.WriteLine("Unfortunately, you did not pass the course. Better luck next time!");
-        }
-        Console.WriteLine("Thank you for using the Journal Project. Goodbye!");
+        
     }
 }
