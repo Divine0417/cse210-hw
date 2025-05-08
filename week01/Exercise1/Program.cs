78bbfc4cd4833c62308d1f2535070210fc6b1142
Divine0417 <divineayobami179@gmail.com>
2025-05-08 05:12:38 +0100

    Prep1 Completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is a simple C# program to demonstrate the structure of a console application.");
+        Console.WriteLine("");
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {firstName}, {lastName} {firstName}.");
     }
 }
