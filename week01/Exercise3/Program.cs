c274e5fe1443b11ad10fdf7645e41049bdbd870f
Divine0417 <divineayobami179@gmail.com>
2025-05-10 15:34:10 +0100

    Part 1 of the assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       
+       Random randomNumberGenerator = new Random();
+       int magicNUmber = randomNumberGenerator.Next(1, 101);
+
+       int guess = -1;
+
+        while (guess != magicNUmber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNUmber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNUmber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!!!");
+            }
+        }
     }
 }
